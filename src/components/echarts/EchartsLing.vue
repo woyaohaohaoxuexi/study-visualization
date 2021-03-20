823ffc008f84aac75c9fa99cd1868cb6ed9f2788
liuning <pactera-440@crc.com.hk>
2021-03-20 12:00:36 +0800

    学习 echarts 绘制图表

--- src/components/echarts/EchartsLing.vue
+++ src/components/echarts/EchartsLing.vue
@@ -15,7 +15,7 @@ export default {
   },
   mounted() {
     this.myEcharts = echarts.init(this.$refs.echarts)
-    this.testOne()
+    this.testTwo()
   },
   methods: {
     // test
@@ -33,6 +33,14 @@ export default {
             ['海水利用业', 2]
           ]
         },
+        grid: {
+          // show: true,
+          left: '10%',
+          // top: 0,
+          containLabel: false,
+          // backgroundColor: '#e2e2e2',
+          // borderColor: 'red'
+        },
         xAxis: {
           show: true,
           type: 'category',
@@ -45,13 +53,52 @@ export default {
           // },
           // nameGap: 20,
           // nameRotate: 30,
-          boundaryGap: true
+          offset: 10,
+          boundaryGap: true, // 柱子立在两个刻度中间。
+          axisLine: { // 坐标轴样式
+            show: false,
+            // lineStyle: {
+            //   color: 'red',
+            // }
+          },
+          axisTick: {   // 坐标轴的刻度
+            show: false,
+            alignWithLabel: true,  // boundaryGap 为 true 时让刻度在柱子的中间
+          },
+          minorTick: {
+            show: false
+          },
+          // splitLine: {  // 分割线，
+          //   show: true,
+          //   lineStyle: {
+          //     type: 'dashed'
+          //   }
+          // },
+          axisPointer: {  // 鼠标放上去的时候，需要展示的东西
+            show: true,
+          }
         },
-        yAxis: {},
+        yAxis: {
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed'
+            }
+          }
+        },
         series: [{
           type: 'bar',
           barWidth: 20,
+          label: {  // 是否需要把数值显示到柱子上面
+            show: false
+          },
+          itemStyle: {
+            color: 'skyblue',
+            // borderColor: 'red',
+            // borderWidth: 1,
+            // borderType: 'dashed'
 
+          }
         }]
       }
       const testOption = {
@@ -79,7 +126,92 @@ export default {
           ]
       }
       this.myEcharts.setOption(options)
-    }
+    },
+    testTwo() {
+      const options = {
+        grid: {
+          left: '10%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          interval: 5,
+          data: ['海洋产业1', '海洋渔业', '海洋盐业', '海洋旅游业']
+        },
+        yAxis: {
+          interval: 2,
+          min: -7,
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed'
+            }
+          }
+        },
+        series: [
+          {
+            type: 'bar',
+            name: 'top',
+            stack: '总数据',
+            itemStyle: {
+              color: 'rgba(0, 0, 0, 0)'
+            },
+            barWidth: 8,
+            itemStyle: {
+              color: 'skyblue',
+              borderRadius: [4, 4, 0, 0]
+            },
+            data: [5, 4, 6, 7]
+          },
+          {
+            type: 'bar',
+            stack: '总数据',  // 相同系列设置了相同的这个值后，就会重叠绘制图形
+            name: 'bottom',
+            itemStyle: {
+              color: 'skyblue',
+              borderRadius: [0, 0, 4, 4]
+            },
+            data: [-1, -2, -4, -2]
+          },
+          {
+            type: 'scatter',
+            symbol: 'circle',
+            symbolSize: 16,
+            itemStyle: {
+              opacity: 1
+            },
+            data: [
+              [0.25, 5],
+              [0.5, 4],
+              [1.75, 6],
+              [3, 7]
+            ]
+          },
+          {
+            type: 'scatter',
+            symbol: 'circle',
+            symbolSize: 16,
+            itemStyle: {
+              opacity: 1
+            },
+            data: [
+              [0.25, -1],
+              [0.5, -2],
+              [1.75, -4],
+              [3, -2]
+            ]
+          }
+        ]
+      }
+      this.myEcharts.setOption(options)
+    },
+    testThree() {}
   }
 }
 </script>
